ba1987202a2a759d9475df7a48892899b9241a97
Ogbajeleo <leonardaromeh@gmail.com>
2025-09-16 03:12:12 +0100

    Made edit on the landing page

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -12,7 +12,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   if (loading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-amber-600"></div>
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary_color"></div>
       </div>
     );
   }
